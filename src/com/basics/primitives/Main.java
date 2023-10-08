50f00062a559d8576c9834732b1d82c6d5d4011f
a.karamiani <aziz.karamiani@gmail.com>
2023-10-09 01:08:26 +0330

    Math Operation

--- src/com/basics/primitives/Main.java
+++ src/com/basics/primitives/Main.java
@@ -3,8 +3,9 @@ package com.basics.primitives;
 public class Main {
     public static void main(String[] args) {
         // Primitives (Integer, Boolean, Char, Double)
-        int number1 = 20;
-        int number2 = 30;
+        System.out.println("****** { Primitives (Integer, Boolean, Char, Double) } ******");
+        int number1 = 10;
+        int number2 = 3;
         int result = number1 + number2;
         System.out.println("Number1 + Number2 = " + result);
 
@@ -16,5 +17,28 @@ public class Main {
 
         double pi = 3.14;
         System.out.println(pi);
+
+        // Math Operations
+        /*
+            +	Addition	Adds together two values	x + y
+            -	Subtraction	Subtracts one value from another	x - y
+            *	Multiplication	Multiplies two values	x * y
+            /	Division	Divides one value by another	x / y
+            %	Modulus	Returns the division remainder	x % y
+            ++	Increment	Increases the value of a variable by 1	++x
+            --	Decrement	Decreases the value of a variable by 1	--x
+         */
+        System.out.println("****** { Math Operations () } ******");
+        System.out.println("Number1 + Number2 = " + (number1 + number2));
+        System.out.println("Number1 - Number2 = " + (number1 - number2));
+        System.out.println("Number1 * Number2 = " + (number1 * number2));
+        System.out.println("Number1 / Number2 = " + (number1 / number2));
+        System.out.println("Number1 ٪ Number2 = " + (number1 % number2));
+
+        System.out.println("Increment by 1 => " + number1++);
+        System.out.println("Increment by 1 => " + ++number1);
+
+        System.out.println("Decrement by 1 => " + number1--);
+        System.out.println("Decrement by 1 => " + --number1);
     }
 }
