b06d3b6c55084a09881b8197d46706b2ac886ed7
a.karamiani <aziz.karamiani@gmail.com>
2023-10-09 00:04:05 +0330

    Primitive data type

---
+++ src/com/basics/primitives/Main.java
@@ -0,0 +1,20 @@
+package com.basics.primitives;
+
+public class Main {
+    public static void main(String[] args) {
+        // Primitives (Integer, Boolean, Char, Double)
+        int number1 = 20;
+        int number2 = 30;
+        int result = number1 + number2;
+        System.out.println("Number1 + Number2 = " + result);
+
+        boolean isAvailable = false;
+        System.out.println(isAvailable);
+
+        char a = 'A';
+        System.out.println(a);
+
+        double pi = 3.14;
+        System.out.println(pi);
+    }
+}
