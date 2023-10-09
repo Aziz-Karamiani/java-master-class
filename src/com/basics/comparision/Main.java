305c22dfdcfba557b7647954e85a04a3ddc69126
a.karamiani <aziz.karamiani@gmail.com>
2023-10-09 18:53:50 +0330

    Comparison Operators

---
+++ src/com/basics/comparision/Main.java
@@ -0,0 +1,13 @@
+package com.basics.comparision;
+
+public class Main {
+    public static void main(String[] args) {
+        // Comparison Operators < > <= >= == !=
+        System.out.println(10 == 10);
+        System.out.println(10 != 10);
+        System.out.println(10 > 10);
+        System.out.println(10 < 10);
+        System.out.println(10 >= 10);
+        System.out.println(10 <= 10);
+    }
+}
