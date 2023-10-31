fee5a84427414d2cc5bb51c24f67229ce2f01f04
a.karamiani <aziz.karamiani@gmail.com>
2023-10-31 16:41:12 +0330

    Java Method

---
+++ src/com/basics/classes/Main.java
@@ -0,0 +1,17 @@
+package com.basics.classes;
+
+public class Main {
+//     Access Modifiers
+//     Optional Static
+//     Return Type
+//     Name
+//     Optional Parameters
+//     Method Body
+    public static void printBrand() {
+        System.out.println("My Brand");
+    }
+
+    public static void main(String[] args) {
+        printBrand();
+    }
+}
